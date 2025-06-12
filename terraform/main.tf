a35eb2fc1e62d246cbc57007ea30a7fb1671864e
Vinicius Aguilar <vinicius.aguilar@premiumads.com.br>
2025-06-12 01:16:21 -0300

    Add force_delete to ECR repository to allow deletion of non-empty repositories

--- terraform/main.tf
+++ terraform/main.tf
@@ -86,6 +86,7 @@ resource "aws_security_group" "ecs_sg" {
 resource "aws_ecr_repository" "app_repo" {
   name                 = "${var.project_name}-app"
   image_tag_mutability = "MUTABLE"
+  force_delete         = true
 
   image_scanning_configuration {
     scan_on_push = true
